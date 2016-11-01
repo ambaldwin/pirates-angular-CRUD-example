ad6ee82a1ee3f6eafa733d0af15313c1938f8dd3
ambaldwin21 <anna.baldwin21@gmail.com>
2016-11-01 15:28:01 -0600

    json object returning

---
+++ public/javascripts/services/pirates_service.js
@@ -0,0 +1,8 @@
+
+app.factory('PiratesService', function ($http) {
+  return {
+    all: function() {
+      return $http.get('/api/pirates');
+    }
+  }
+})
--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  knex('pirates').then((pirates)=> {
+    res.json(pirates)
+    console.log(pirates);
+  })
 });
 
 module.exports = router;
