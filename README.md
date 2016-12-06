c7213f12b55f05a17b4dd8715c0eb82db7715692
Anna Baldwin <anna.baldwin21@gmail.com>
2016-12-06 14:58:14 -0700

    readme

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # pirates-angular-CRUD-example
 
-A basic CRUD app example using the NEAP stack - Node, Express, Angular, PostgreSQL. Users can create, update, and delete the pirate objects.
+A basic CRUD app example using the NEAP stack - Node, Express, Angular, PostgreSQL. Users can create, update, and delete the blog-post style Pirates. ARGH!
+
+##Features
+1. On the home page, the 'Add New Pirate' button makes a form appear where users can add in information to form a new blog-type post with the pirate's information.
+
+![screen shot 2016-12-06 at 2 53 43 pm](https://cloud.githubusercontent.com/assets/18018191/20945341/170f8c6c-bbc4-11e6-99bf-b594f8a65add.png)
+
+2. Users can click into each individual pirate (the pirates' names are links) to go to the pirate's individual page. Here, users can edit the post and also delete the post.
+
+![screen shot 2016-12-06 at 2 54 15 pm](https://cloud.githubusercontent.com/assets/18018191/20945396/5967f432-bbc4-11e6-876d-d148c2f7bd71.png)
--- server/routes/api.js
+++ server/routes/api.js
@@ -4,50 +4,50 @@ var knex = require('../db/knex');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    knex('pirates').then((pirates) => {
-        res.json(pirates)
-    })
+   knex('pirates').then((pirates) => {
+       res.json(pirates)
+   })
 });
 
 router.get('/:id', (req, res, next) => {
-    if (Number.parseInt(req.params.id)) {
-        knex('pirates').where('pirates.id', req.params.id).first()
-            .then((pirate) => {
-                res.json(pirate);
-            })
-    }
+   if (Number.parseInt(req.params.id)) {
+       knex('pirates').where('pirates.id', req.params.id).first()
+           .then((pirate) => {
+               res.json(pirate);
+           })
+   }
 
 });
 
 router.post('/', (req, res, next) => {
-    let newPirate = {
-        name: req.body.name,
-        poison: req.body.poison,
-        image: req.body.image,
-        accessory: req.body.accessory
-    }
-    knex('pirates').insert(newPirate, '*')
-        .then((newPirate) => {
-            res.json(newPirate);
-        })
+   let newPirate = {
+       name: req.body.name,
+       poison: req.body.poison,
+       image: req.body.image,
+       accessory: req.body.accessory
+   }
+   knex('pirates').insert(newPirate, '*')
+       .then((newPirate) => {
+           res.json(newPirate);
+       })
 });
 
 router.put('/:id', function(req, res, next) {
-    knex('pirates').where('id', req.body.id).first()
-        .update(req.body)
-        .then(function(pirate) {
-            res.json('pirate updated')
-        })
+   knex('pirates').where('id', req.body.id).first()
+       .update(req.body)
+       .then(function(pirate) {
+           res.json('pirate updated')
+       })
 })
 
 router.delete('/:id', (req, res, next) => {
-    knex('pirates')
-        .where('pirates.id', req.params.id).delete()
-        .then(() => {
-            res.json({
-                'response': 'post deleted'
-            })
-        })
+   knex('pirates')
+       .where('pirates.id', req.params.id).delete()
+       .then(() => {
+           res.json({
+               'response': 'post deleted'
+           })
+       })
 })
 
 
